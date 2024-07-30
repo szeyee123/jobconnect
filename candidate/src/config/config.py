742fb953b3515215312aa95c5af803a1e12c53ca
szeyee123 <77826245+szeyee123@users.noreply.github.com>
2024-07-31 00:52:48 +0800

    Update config.py

--- candidate/src/config/config.py
+++ candidate/src/config/config.py
@@ -2,5 +2,5 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-MONGO_URI = os.environ["MONGO_URI"]
+MONGO_URI = os.environ["mongodb+srv://szeyeetew:b1Mc9uFyOQGg8KTS@cluster1.obeeiyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"]
 ENV = os.environ["ENV"]
