cc700762a154f50097a6e763850c495abd0d17d9
szeyee <szeyee.tew@the-elxr.com>
2024-08-01 22:32:55 +0800

    connect to mongodb

--- candidate/src/config/config.py
+++ candidate/src/config/config.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 load_dotenv()
 
 MONGO_URI = os.environ["MONGO_URI"]
 ENV = os.environ["ENV"]
+
+print(f"MongoDB URI: {MONGO_URI}")
